02babbb48d9350fc0481d69249fe955167d5adfe
Chris DeRose <cderose@Chriss-MacBook-Air.local>
2013-07-23 23:00:07 -0400

    Finished up moving all the tables over to the new helper method

--- NavigatorMaxFrontend/app/helpers/application_helper.rb
+++ NavigatorMaxFrontend/app/helpers/application_helper.rb
@@ -5,23 +5,27 @@ module ApplicationHelper
 
   def data_table( title, rows, options = {})
     legends_for = options[:legends] || []
-    columns = options[:columns] || ['','Value'] 
+    columns = options[:columns] || ['Value'] 
+    row_header = ''
 
     [ '<h3>%s</h3>' % title,
       '<table class="%s" id="%s">' % [ 
         %w(table table-striped table-bordered table-condensed).join(' '), 
         underscore2(title)
       ],
-      '<thead><tr>%s</tr></thead>' % columns.collect{|c| '<th>%s</th>' % c }.join,
+      '<thead><tr>%s</tr></thead>' % ([row_header]+columns).collect{|c| '<th>%s</th>' % c }.join,
       '<tbody>',
       rows.collect{|r| 
         shorthand = underscore2(r)
-        '<tr><th>%s</th><td class="%s"></td></tr>' % [
+
+        '<tr><th>%s</th>%s</tr>' % [
           [(legends_for.include?(shorthand) ) ? legend_for(r) : nil, r].compact.join,
-          shorthand ]
+          columns.collect{|col| '<td class="%s"></td>' % [ shorthand, 
+            (columns.length > 1) ? underscore2(col) : nil ].compact.join('_')  }.join
+        ]
       },
       '</tbody>',
-      '</table>' ].join.html_safe
+      '</table>' ].flatten.join.html_safe
   end
 
   def legend_for(column)
@@ -33,6 +37,6 @@ module ApplicationHelper
   # Not a creative name, but this works a bit better for us than the default rails
   # underscore
   def underscore2(val)
-    val.downcase.tr(' ','_').tr('^a-z0-9_','')
+    val.downcase.tr('^a-z0-9_ ','').strip.tr(' ','_')
   end
 end
